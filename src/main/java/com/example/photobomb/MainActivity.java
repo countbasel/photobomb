58d046c34be398a476fbcad4da6adc4f27700f66
countbasel <mjbasel@gmail.com>
2013-05-31 17:55:08 -0500

    first upload
    yay!

---
+++ src/main/java/com/example/photobomb/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.photobomb;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.photobomb.R;
+
+public class MainActivity extends Activity {
+    public final static String DEBUG_TAG = "MakePhotoActivity";
+    private Camera camera;
+    private int cameraId = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // do we have a camera?
+        if (!getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            Toast.makeText(this, "No camera on this device", Toast.LENGTH_LONG)
+                    .show();
+        } else {
+            cameraId = findBackFacingCamera();
+            if (cameraId < 0) {
+                Toast.makeText(this, "No back facing camera found.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                camera = Camera.open(cameraId);
+            }
+        }
+    }
+
+    public void onClick(View view) {
+        //ImageView imageView= (ImageView) findViewById(R.id.result);
+        if(camera == null) {
+            // Warn user that camera is not available via "Toast" or similar.
+            setContentView(R.layout.activity_main);
+
+            TextView t = new TextView(this);
+
+            t=(TextView)findViewById(R.id.text01);
+            t.setText("Arrrrrrgggggggg");
+
+        } else {
+            camera.takePicture(null, null,
+                    new PhotoHandler(getApplicationContext()));
+
+        }
+    }
+
+    private int findBackFacingCamera() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
+                Log.d(DEBUG_TAG, "Camera found");
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    @Override
+    protected void onPause() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+        super.onPause();
+    }
+
+}
